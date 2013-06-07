3dd1430e1ec281e9a437c4e981d08515f0c76b2e
Dave Cheney <dave@cheney.net>
2013-06-07 10:55:35 +0000

    Split gpio functions into their own file.

--- rpi.go
+++ rpi.go
@@ -3,8 +3,6 @@ package rpi
 import (
 	"log"
 	"os"
-	"syscall"
-	"unsafe"
 )
 
 const (
@@ -86,63 +84,11 @@ const (
 	GPIO_P1_22 = 25
 )
 
-var (
-	gpfsel, gpset, gpclr, gplev []*uint32
-)
-
 func init() {
 	memfd, err := os.OpenFile("/dev/mem", os.O_RDWR|os.O_SYNC, 0)
 	if err != nil {
 		log.Fatalf("rpi: unable to open /dev/mem: %v", err)
 	}
-	buf, err := syscall.Mmap(int(memfd.Fd()), BCM2835_GPIO_BASE, BCM2835_BLOCK_SIZE, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
-	if err != nil {
-		log.Fatalf("rpi: unable to mmap GPIO page: %v", err)
-	}
-	gpfsel = []*uint32{
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL0])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL1])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL2])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL3])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL4])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPFSEL5])),
-	}
-	gpset = []*uint32{
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPSET0])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPSET1])),
-	}
-	gpclr = []*uint32{
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPCLR0])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPCLR1])),
-	}
-	gplev = []*uint32{
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPLEV0])),
-		(*uint32)(unsafe.Pointer(&buf[BCM2835_GPLEV1])),
-	}
-}
-
-func GPIOFSel(pin, mode uint8) {
-	offset := pin / 10
-	shift := (pin % 10) * 3
-	mask := BCM2835_GPIO_FSEL_MASK << shift
-	value := uint32(mode) << shift
-	*gpfsel[offset] = value & mask
-}
-
-func GPIOSet(pin uint8) {
-	offset := pin / 32
-	shift := pin % 32
-	*gpset[offset] = (1 << shift)
-}
-
-func GPIOClear(pin uint8) {
-	offset := pin / 32
-	shift := pin % 32
-	*gpclr[offset] = (1 << shift)
-}
-
-func GPIOGet(pin uint8) bool {
-	offset := pin / 32
-	shift := pin % 32
-	return *gplev[offset]&(1<<shift) == (1 << shift)
+	initGPIO(int(memfd.Fd()))
+	memfd.Close()
 }
